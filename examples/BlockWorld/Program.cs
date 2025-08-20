7e9947f6583f7dff1a40efab51f8b6a094d2be8e
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-08-19 22:57:17 -0400

    Added BlockWorld and removed swiftNote

---
+++ examples/BlockWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SwiftSharp;
+using SwiftSharp.Foundation;
+
+namespace BlockWorld
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
