5ba1ba516ff7f377bf1ba2805186d11d1a4a4d07
Aaron Thompson <athompsonbriley.56@gmail.com>
2025-08-28 18:31:01 -0400

    Update .gitignore, remove OSConfig.props, and enhance documentation structure

--- examples/Sandbox/Program.cs
+++ examples/Sandbox/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using SwiftSharp;
+using SwiftSharp.Foundation;
 
 internal class Program
 {
     public static void Main()
     {
-        Glibc
+        Glibc.Printf("Hello, World!\n");
+        ulong length = Glibc.Strlen($"Hello, {10}!");
+        Console.WriteLine($"Length of 'Hello, World!': {length}");
     }
 }
